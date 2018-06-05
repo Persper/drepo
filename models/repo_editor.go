cea37e4848e3043dd3c14ce6c91daa96c4afa99d
Jinglei Ren <jinglei@persper.org>
2018-06-05 20:48:21 +0800

    License our code under MPL to enforce open-souce derivatives

--- models/repo_editor.go
+++ models/repo_editor.go
@@ -1,3 +1,8 @@
+// Copyright 2018 Persper Foundation
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
 // Copyright 2016 The Gogs Authors. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
